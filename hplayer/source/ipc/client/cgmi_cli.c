f253e1da702e4558061a87985ac3fdce20b55ff3
Kris Kersey <kkersey@cisco.com>
2013-11-21 13:21:58 -0500

    cgmi_cli updates for new APIs.
    Switched error strings to new cgmi_ErrorString() interface.
    Added the following APIs: cgmi_SetPosition(), cgmi_GetRateRange(),
    cgmi_SetRate(), cgmi_GetDuration()
    Added check on cgmi_canPlayType() before play.

--- hplayer/source/ipc/client/cgmi_cli.c
+++ hplayer/source/ipc/client/cgmi_cli.c
@@ -2,34 +2,9 @@
 #include <glib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "cgmiPlayerApi.h"
-
-/* Error Strings */
-const char *strError[] =
-{
-    "CGMI_ERROR_SUCCESS",
-    "CGMI_ERROR_FAILED",
-    "CGMI_ERROR_NOT_IMPLEMENTED",
-    "CGMI_ERROR_NOT_SUPPORTED",
-    "CGMI_ERROR_BAD_PARAM",
-    "CGMI_ERROR_OUT_OF_MEMORY",
-    "CGMI_ERROR_TIMEOUT",
-    "CGMI_ERROR_INVALID_HANDLE",
-    "CGMI_ERROR_NOT_INITIALIZED",
-    "CGMI_ERROR_NOT_OPEN",
-    "CGMI_ERROR_NOT_ACTIVE",
-    "CGMI_ERROR_NOT_READY",
-    "CGMI_ERROR_NOT_CONNECTED",
-    "CGMI_ERROR_URI_NOTFOUND",
-    "CGMI_ERROR_WRONG_STATE"
-};
-#define DUMP_ERROR(err)    \
-   do\
-      {  \
-         printf("%s\n", strError[err]);  \
-      }  \
-      while(0) 
 
 /* Prototypes */
 static void cgmiCallback( void *pUserData, void *pSession, tcgmi_Event event );
@@ -79,11 +54,69 @@ static cgmi_Status getposition(void *pSessionId, float *pPosition)
     retCode = cgmi_GetPosition( pSessionId, pPosition );
     if (retCode != CGMI_ERROR_SUCCESS)
     {
-        printf("CGMI GetPosition failed\n");
+        printf("CGMI GetPosition Failed\n");
     }
 
     return retCode;
 }
+
+/* SetPosition Command */
+static cgmi_Status setposition(void *pSessionId, float Position)
+{
+    cgmi_Status retCode = CGMI_ERROR_SUCCESS;
+
+    retCode = cgmi_SetPosition( pSessionId, Position );
+    if (retCode != CGMI_ERROR_SUCCESS)
+    {
+        printf("CGMI SetRate Failed\n");
+    }
+
+    return retCode;
+}
+
+/* GetRateRange Command */
+static cgmi_Status getraterange(void *pSessionId, float *pRewind, float *pFFoward)
+{
+    cgmi_Status retCode = CGMI_ERROR_SUCCESS;
+
+    retCode = cgmi_GetRateRange( pSessionId, pRewind, pFFoward );
+    if (retCode != CGMI_ERROR_SUCCESS)
+    {
+        printf("CGMI GetRateRange Failed\n");
+    }
+
+    return retCode;
+}
+
+/* SetRate Command */
+static cgmi_Status setrate(void *pSessionId, float Rate)
+{
+    cgmi_Status retCode = CGMI_ERROR_SUCCESS;
+
+    retCode = cgmi_SetRate( pSessionId, Rate );
+    if (retCode != CGMI_ERROR_SUCCESS)
+    {
+        printf("CGMI SetRate Failed\n");
+    }
+
+    return retCode;
+}
+
+/* GetDuration Command */
+static cgmi_Status getduration(void *pSessionId, float *pDuration, cgmi_SessionType *type)
+{
+    cgmi_Status retCode = CGMI_ERROR_SUCCESS;
+
+    retCode = cgmi_GetDuration( pSessionId, pDuration, type );
+    if (retCode != CGMI_ERROR_SUCCESS)
+    {
+        printf("CGMI GetDuration Failed\n");
+    }
+
+    return retCode;
+}
+
+
 
 /* Callback Function */
 static void cgmiCallback( void *pUserData, void *pSession, tcgmi_Event event )
@@ -101,14 +134,21 @@ int main(int argc, char **argv)
     gchar arg[512];
     gint quit = 0;
     gint playing = 0;
+
+    /* Status Variables */
     gfloat Position = 0.0;
+    gfloat Rate = 0.0;
+    gfloat Rewind = 0.0;
+    gfloat FFoward = 0.0;
+    gfloat Duration = 0.0;
+    cgmi_SessionType type = cgmi_Session_Type_UNKNOWN;
+    gint pbCanPlay = 0;
 
     /* Init CGMI. */
     retCode = cgmi_Init();
     if(retCode != CGMI_ERROR_SUCCESS)
     {
-        printf("CGMI Init Failed\n");
-        DUMP_ERROR(retCode);
+        printf("CGMI Init Failed: %s\n", cgmi_ErrorString( retCode ));
         return 1;
     } else {
         printf("CGMI Init Success!\n");
@@ -118,8 +158,7 @@ int main(int argc, char **argv)
     retCode = cgmi_CreateSession( cgmiCallback, NULL, &pSessionId );
     if (retCode != CGMI_ERROR_SUCCESS)
     {
-        printf("CGMI CreateSession failed\n");
-        DUMP_ERROR(retCode);
+        printf("CGMI CreateSession Failed: %s\n", cgmi_ErrorString( retCode ));
         return 1;
     } else {
         printf("CGMI CreateSession Success!\n");
@@ -131,7 +170,15 @@ int main(int argc, char **argv)
     printf("Supported commands:\n"
            "\tplay <url>\n"
            "\tstop (or unload)\n"
+           "\n"
+           "\tgetraterange\n"
+           "\tsetrate <rate (float)>\n"
+           "\n"
            "\tgetposition\n"
+           "\tsetposition <position (seconds) (float)>\n"
+           "\n"
+           "\tgetduration\n"
+           "\n"
            "\tquit\n\n");
 
     /* Main Command Loop */
@@ -152,11 +199,29 @@ int main(int argc, char **argv)
         {
             strncpy( arg, command + 5, strlen(command) - 6 );
             arg[strlen(command) - 6] = '\0';
-            printf("Playing \"%s\"...\n", arg);
-            play(pSessionId, arg);
-            if ( retCode == CGMI_ERROR_SUCCESS )
+            /* Check First */
+            printf("Checking if we can play this...");
+            retCode = cgmi_canPlayType( arg, &pbCanPlay );
+            if ( retCode == CGMI_ERROR_NOT_IMPLEMENTED )
             {
-                playing = 1;
+                printf( "cgmi_canPlayType Not Implemented\n" );
+                printf("Playing \"%s\"...\n", arg);
+                retCode = play(pSessionId, arg);
+                if ( retCode == CGMI_ERROR_SUCCESS )
+                {
+                    playing = 1;
+                }
+            } else if ( !retCode && pbCanPlay )
+            {
+                printf( "Yes\n" );
+                printf("Playing \"%s\"...\n", arg);
+                retCode = play(pSessionId, arg);
+                if ( retCode == CGMI_ERROR_SUCCESS )
+                {
+                    playing = 1;
+                }
+            } else {
+                printf( "No\n" );
             }
         }
         /* stop or unload */
@@ -169,6 +234,24 @@ int main(int argc, char **argv)
                 playing = 0;
             }
         }
+        /* getraterange */
+        else if (strncmp(command, "getraterange", 12) == 0)
+        {
+            retCode = getraterange(pSessionId, &Rewind, &FFoward);
+            if (!retCode)
+            {
+                printf( "Rate Range: %f : %f\n", Rewind, FFoward );
+            }
+        }
+        /* setrate */
+        else if (strncmp(command, "setrate", 7) == 0)
+        {
+            strncpy( arg, command + 8, strlen(command) - 9 );
+            arg[strlen(command) - 9] = '\0';
+            Rate = (float) atof(arg);
+            printf( "Setting rate to %f (%s)\n", Rate, arg );
+            retCode = setrate(pSessionId, Rate);
+        }
         /* getposition */
         else if (strncmp(command, "getposition", 11) == 0)
         {
@@ -176,6 +259,42 @@ int main(int argc, char **argv)
             if (!retCode)
             {
                 printf( "Position: %f\n", Position );
+            }
+        }
+        /* setposition */
+        else if (strncmp(command, "setposition", 11) == 0)
+        {
+            strncpy( arg, command + 12, strlen(command) - 13 );
+            arg[strlen(command) - 13] = '\0';
+            Position = (float) atof(arg);
+            printf( "Setting position to %f (%s)\n", Position, arg );
+            retCode = setposition(pSessionId, Position);
+        }
+        /* setposition */
+        else if (strncmp(command, "getduration", 11) == 0)
+        {
+            retCode = getduration(pSessionId, &Duration, &type);
+            if (!retCode)
+            {
+                printf( "Duration: %f, SessionType: ", Duration );
+                switch( type )
+                {
+                    case LIVE:
+                        printf( "LIVE\n" );
+                        break;
+                    case TSB:
+                        printf( "TSB\n" );
+                        break;
+                    case FIXED:
+                        printf( "FIXED\n" );
+                        break;
+                    case cgmi_Session_Type_UNKNOWN:
+                        printf( "cgmi_Session_Type_UNKNOWN\n" );
+                        break;
+                    default:
+                        printf( "ERROR\n" );
+                        break;
+                }
             }
         }
         /* quit */
@@ -187,7 +306,7 @@ int main(int argc, char **argv)
         /* If we receive and error, print error. */
         if (retCode)
         {
-            DUMP_ERROR(retCode);
+            printf( "Error: %s\n", cgmi_ErrorString( retCode ) );
         }
     }
 
@@ -201,8 +320,7 @@ int main(int argc, char **argv)
     retCode = cgmi_DestroySession( pSessionId );
     if (retCode != CGMI_ERROR_SUCCESS)
     {
-        printf("CGMI DestroySession failed\n");
-        DUMP_ERROR(retCode);
+        printf("CGMI DestroySession Failed: %s\n", cgmi_ErrorString( retCode ));
     } else {
         printf("CGMI DestroySession Success!\n");
     }
@@ -211,8 +329,7 @@ int main(int argc, char **argv)
     retCode = cgmi_Term();
     if (retCode != CGMI_ERROR_SUCCESS)
     {
-        printf("CGMI Term failed\n");
-        DUMP_ERROR(retCode);
+        printf("CGMI Term Failed: %s\n", cgmi_ErrorString( retCode ));
     } else {
         printf("CGMI Term Success!\n");
     }
